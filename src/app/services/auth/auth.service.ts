7285677bae2ab4b6b5b14c2e1cad43c28f29a163
Bernardo Gutierrez <bernardoc.gutierrezc@gmail.com>
2020-07-14 11:47:09 -0500

    documented auth service and updated interface

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -6,7 +6,7 @@ import { auth} from 'firebase/app';
 // Firestore
 import { AngularFireAuth} from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
-import { BehaviorSubject, Subscription} from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 
@@ -14,55 +14,91 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  private subscriptionToUser: Subscription = new Subscription();
-  user$: BehaviorSubject<User> = new BehaviorSubject<User>(null);
-  uid$: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+  private uid$ = new BehaviorSubject<string>(null);
 
   constructor(
     private afAuth: AngularFireAuth,
     private afs: AngularFirestore,
     private router: Router
   ) {
-    this.afAuth.onAuthStateChanged(user => {
-      if (user) {
-        this.subscriptionToUser.add(
-          this.afs.doc<User>(`users/${user.uid}`)
-                  .valueChanges()
-                  .subscribe(this.user$)
-        );
-        this.uid$.next(user.uid);
-      } else {
-        this.subscriptionToUser.unsubscribe();
-        this.user$.next(null);
-        this.uid$.next(null);
-      }
-    });
+    this.afAuth.authState.subscribe(user =>
+      this.uid$.next(user ? user.uid : null)
+    );
   }
 
-  isLoggedIn(): boolean {
-    return this.uid$.value !== null;
+  /**
+   * Creates an observable for the user id of the currently logged in user.
+   * @return Observable that immediately returns the uid of the current user
+   *         (or null, if not signed in) and listens to changes in auth state
+   */
+  getUid(): Observable<string> {
+    return this.uid$.asObservable();
   }
 
+  /**
+   * Creates an observable for whether the user is logged in.
+   * @return Observable that immediately returns whether the user is logged in
+   *         and listens to changes in auth state
+   */
+  getLoggedIn(): Observable<boolean> {
+    return this.uid$.asObservable().pipe(
+      map(uid => uid !== null)
+    );
+  }
+
+  /**
+   * Promise that fetches the data of current user from Firebase.
+   * @async
+   * @return Promise that resolves to a snapshot of the current user data
+   */
+  getUser(): Promise<User> {
+    return this.getUserData(this.uid$.value);
+  }
+
+  /**
+   * Opens a popup for the user to authenticate with the Google provider.
+   * If this is a new user (hasn't signed in before), a new default user
+   * profile is created in Firebase with the property isNewUser = true.
+   * @async
+   * @return Promise that returns the user data (if user exists) or the newly
+   *         created user (if it didn't exist).
+   */
   async signinWithGoogle(): Promise<User> {
     const provider = new auth.GoogleAuthProvider()
                              .setCustomParameters({prompt: 'select_account'});
 
     const credential = await this.afAuth.signInWithPopup(provider);
     const authorization = credential.additionalUserInfo;
-    const userData = (authorization.isNewUser) ?
+    return (authorization.isNewUser) ?
         await this.createUser(credential.user.uid, authorization.profile) :
         await this.getUserData(credential.user.uid);
-
-    this.user$.next(userData);
-    return userData;
   }
 
-  async signOut(): Promise<boolean> {
-    await this.afAuth.signOut();
-    return this.router.navigate(['/']);
+  /**
+   * Signs out the current user and navigates the router to the root route if successful
+   * @async
+   * @return Promise that returns whether the signout was successful
+   */
+  async signout(): Promise<boolean> {
+    return this.afAuth.signOut().then(() =>
+      this.router.navigate(['/'])
+    );
   }
 
+  /**
+   * Creates a new user in Firebase parsing the information from the profile returned by the
+   * provider during sign in. Uses a regex to determine if the email is from a Tec student
+   * and parses their matricula from it.
+   * @async
+   * @param  uid     Id if the user to fetch (due to backend rules, can only be the currentle
+   *                 authenticated user or the call will fail)
+   * @param  profile The profile returned by the provider used to sign in
+   * @return         Promise that returns the newly created user's data
+   */
   private createUser(uid: string, profile: any): Promise<User> {
+    // Check if uid was provided
+    if (!uid) { return new Promise(null); }
+
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${uid}`);
     const data: User = {
       uid,
@@ -73,11 +109,27 @@ export class AuthService {
       following : [],
       isNewUser : true,
     };
+    // Check if email is of a tec student (Axxxxxxx@itesm.mx o axxxxxxx@itesm.mx)
+    if (/[aA]\d{8}@itesm.mx/.test(data.email)) {
+      data.matricula = data.email.substr(0, 9).toUpperCase();
+    }
     // Return promise with user data if success, else return null promise
     return userRef.set(data).then(() => data, () => null);
   }
 
+  /**
+   * Creates a new user in Firebase parsing the information from the profile returned by the
+   * provider during sign in. Uses a regex to determine if the email is from a Tec student
+   * and parses their matricula from it.
+   * @async
+   * @param  uid     Id if the user to fetch (due to backend rules, can only be the currentle
+   *                 authenticated user or the call will fail)
+   * @return         Promise that returns the newly created user's data
+   */
   private getUserData(uid: string): Promise<User> {
+    // Check if uid was provided
+    if (!uid) { return new Promise(null); }
+
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${uid}`);
     return userRef.get().pipe(
       map(doc => {
