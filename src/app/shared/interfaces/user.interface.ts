05444934f622b0ee49f435a3a7552ee684a93a48
Bernardo Gutierrez <bernardoc.gutierrezc@gmail.com>
2020-07-14 11:47:42 -0500

    commented user interface

--- src/app/shared/interfaces/user.interface.ts
+++ src/app/shared/interfaces/user.interface.ts
@@ -4,10 +4,13 @@ export interface User {
   lName: string;
   email: string;
   roles: string[];
+  following: string[];
+  isNewUser: boolean;
+  // Atributos para alumnos Tec
+  model?: string;
   major?: string;
   matricula?: string;
   semester?: number;
+  // Atributos para administradores de grupos
   administra?: string[];
-  following: string[];
-  isNewUser: boolean;
 }
