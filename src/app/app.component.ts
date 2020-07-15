3cf172fc8fd3e7cb7648f8449ce8ef7c6bd845db
Bernardo Gutierrez <bernardoc.gutierrezc@gmail.com>
2020-07-14 21:34:27 -0500

    Added sidenav auto-close and ripple effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { AuthService } from './services/auth/auth.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +13,29 @@ import { AuthService } from './services/auth/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'CSAPP';
-  onMobile: Observable<BreakpointState>;
+  onMobile = new BehaviorSubject<BreakpointState>({matches: false, breakpoints: {}});
   loggedIn: Observable<boolean>;
+  @ViewChild('drawer') sidenav: MatSidenav;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    public auth: AuthService
+    public auth: AuthService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
     this.loggedIn = this.auth.getLoggedIn();
-    this.onMobile = this.breakpointObserver.observe(['(max-width: 599px)']);
+    this.breakpointObserver.observe(['(max-width: 599px)'])
+                           .subscribe(this.onMobile);
   }
+
+  navigateByUrl(url: string): void {
+    // Navigate to url
+    this.router.navigateByUrl(url);
+    // If on mobile, close sidenav on click
+    if (this.onMobile.value.matches) {
+      this.sidenav.close();
+    }
+  }
+
 }
