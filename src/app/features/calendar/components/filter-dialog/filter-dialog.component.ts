d4ba9d6d53079cba3ba0440861febe6edd4b53c1
Bernardo Gutierrez <bernardoc.gutierrezc@gmail.com>
2020-09-06 20:00:05 -0500

    solved filter reset problem

--- src/app/features/calendar/components/filter-dialog/filter-dialog.component.ts
+++ src/app/features/calendar/components/filter-dialog/filter-dialog.component.ts
@@ -11,19 +11,33 @@ import { Event } from 'src/app/shared/interfaces/event.interface';
   styleUrls: ['./filter-dialog.component.css']
 })
 export class FilterDialogComponent implements OnInit {
-  areasList = areasList;
+  data: {value: string, color: string, selected: boolean, text: string}[] = null;
   @ViewChild('selAreas') selAreasRef: MatSelectionList;
 
   constructor(
     public dialogRef: MatDialogRef<FilterDialogComponent>,
     public calendarS: CalendarService
-  ) { }
+  ) {
+    // Initialize value of filter
+    const filter = this.calendarS.getFilterSnapshot().areasT21;
+    this.data = Object.values(areasList.Tec21).map(area => ({
+      value: area.aid,
+      color: area.color,
+      selected: filter.includes(area.aid),
+      text: area.aid + ' - ' + area.name,
+    }));
+  }
 
   ngOnInit(): void { }
 
   onChangeSelAreas(event: MatSelectionListChange): void {
     const areas = event.source.selectedOptions.selected.map(op => op.value);
     this.calendarS.setFilter({areasT21: areas});
+  }
+
+  onClear(): void {
+    this.selAreasRef.deselectAll();
+    this.calendarS.setFilter({areasT21: []});
   }
 
   onClose(): void {
