7b7a02b3921f5fe9e547a65bd79c4ac06834c595
Kevin Fernandez <kevinfdz98@gmail.com>
2020-09-08 19:33:21 -0500

    deployed version 08/09/2020

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBCLcF-4MOjDEgETLgyJGwq9hj9G8RggiM",
+    authDomain: "csapp2020-8620b.firebaseapp.com",
+    databaseURL: "https://csapp2020-8620b.firebaseio.com",
+    projectId: "csapp2020-8620b",
+    storageBucket: "csapp2020-8620b.appspot.com",
+    messagingSenderId: "584704330978",
+    appId: "1:584704330978:web:1831648fcd7101d2cc22bb"
+  }
 };
